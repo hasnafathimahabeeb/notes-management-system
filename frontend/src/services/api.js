129cf244a27d715daaedbc753478eb26ba7759f8
hasnafathimahabeeb <shibanashibu73@gmail.com>
2026-06-20 03:11:41 +0530

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: baseURL: "https://notes-management-system-uyrv.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
