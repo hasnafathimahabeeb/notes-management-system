d8bf9cf20c289d1db55d1f7f7e4c3e0b8ca57388
hasnafathimahabeeb <shibanashibu73@gmail.com>
2026-06-20 03:23:45 +0530

    Fix API URL syntax

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: baseURL: "https://notes-management-system-uyrv.onrender.com/api",
+  baseURL: "https://notes-management-system-uyrv.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
